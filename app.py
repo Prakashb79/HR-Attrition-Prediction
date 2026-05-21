b3efe386a85dbfdd13f66ea55aa1e73d864a2ecc
Prakashb79 <hazardftwop@gmail.com>
2026-05-22 00:39:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,8 @@
 import streamlit as st
-import pickle
-import pandas as pd
 
-# Load model
-model = pickle.load(open("model.pkl", "rb"))
+st.title("HR Attrition Prediction App")
 
-st.title("HR Attrition Prediction")
-
-# Input
-age = st.number_input("Age", 18, 60, 25)
+age = st.number_input("Enter Age", 18, 60, 25)
 
 if st.button("Predict"):
-
-    input_data = pd.DataFrame({
-        "Age": [age]
-    })
-
-    prediction = model.predict(input_data)
-
-    st.success(f"Prediction: {prediction[0]}")
+    st.success(f"App Working Successfully. Age entered: {age}")
