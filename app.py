8bb631800774389ccacd4033e4db63be3c293771
Prakashb79 <hazardftwop@gmail.com>
2026-05-22 01:23:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,10 +199,14 @@ if st.button("🔍 Predict Attrition"):
         )
 
         # Predict
-        probability = model.predict_proba(input_data_encoded)[0][1]
+        # Predict Probability
+probability = model.predict_proba(input_data_encoded)[0][1]
+
+st.markdown("## Prediction Result")
 
 st.write(f"Attrition Probability: {probability*100:.2f}%")
 
+# Result
 if probability > 0.35:
 
     st.error("⚠️ Employee is likely to leave the company")
